5c762f56edb463b68b6599bfe19520ebf5176b6c
Kale <125678484@qq.com>
2021-10-09 16:46:27 +0800

    尝试让compose 和viewmodel建立关系。

--- app/src/main/java/com/kale/compose/study/MainActivity.kt
+++ app/src/main/java/com/kale/compose/study/MainActivity.kt
@@ -1,23 +1,28 @@
 package com.kale.compose.study
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kale.compose.study.ui.theme.ComposestudyTheme
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposestudyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    DefaultPreview()
                 }
             }
         }
@@ -29,10 +34,11 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun DefaultPreview() {
+fun DefaultPreview(viewModel: MainViewModel = viewModel()) {
     ComposestudyTheme {
-        Greeting("Android")
+        Greeting(viewModel.data)
+        Log.d("aaa",viewModel.data)
     }
 }
---
+++ app/src/main/java/com/kale/compose/study/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.kale.compose.study
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application): AndroidViewModel(application) {
+
+
+    val data: String ="123"
+}
